f2bd8060066f2fd75bb55e28cb837090a6e24109
usmangq12 <usmangq12@gmail.com>
2022-03-01 20:17:13 +0500

    Designed currency exchange rates

--- src/assets/styles/table.js
+++ src/assets/styles/table.js
@@ -5,12 +5,29 @@ export const Container = styled.div`
 
   table {
     border-spacing: 0;
-    border: 1px solid black;
+    border: 1px solid #ddd;
+    margin-bottom: 1rem;
 
+    thead{
+      background-color: #00be28;
+      color: #fff;
+    }
     tr {
       :last-child {
         td {
           border-bottom: 0;
+        }
+      }
+    }
+    tr {
+      margin-bottom: 1rem;
+      padding: 10.5rem;
+      :nth-child(even) {
+        td {
+          background: #E7E9EB;
+        }
+        td{
+          width: 100%;
         }
       }
     }
@@ -19,8 +36,8 @@ export const Container = styled.div`
     td {
       margin: 0;
       padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
+      border-bottom: 1px solid #E7E9EB;
+      border-right: 1px solid #ddd;
 
       :last-child {
         border-right: 0;
